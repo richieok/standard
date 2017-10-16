c9dc8affa1ac2f048b2d798910482290022f29ec
richieok <richie.okoro@gmail.com>
2017-10-16 18:46:21 +0100

    added mapFolder func to handy module

--- handy.py
+++ handy.py
@@ -1,3 +1,24 @@
 #handy module
 import re, os.path
 from configparser import ConfigParser
+
+class QuitException(Exception):
+    pass
+
+def testQuit(response):
+    '''Raises QuitException when input == "quit"'''
+    temp = input(response)
+    qmo = re.search(r'^quit$', temp, re.I)
+    if qmo:
+        raise QuitException("Quit")
+    else:
+        return temp
+
+def mapFolder(location):
+    m = []
+    for folder, subs, files in os.walk(location):
+        for sub in subs:
+            m.append({os.path.join(folder,sub):False})
+        for f in files:
+            m.append({os.path.join(folder,f):False})
+    return m
